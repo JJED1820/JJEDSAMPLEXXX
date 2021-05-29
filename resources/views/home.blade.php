4e21a1112374b30af03cefb9185e45b20450c78b
JJED1820 <djeanicajoy@gmail.com>
2021-05-29 14:29:51 +0800

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  
+    <div class="row">
+       <div class="col-3 p-5">
+            <img src="/img/logo.png" class="w=100">
+        </div>
+        <div class="col-9 p-5">
+
+            <div class="d-flex align-items-center pb-4">
+                <h1>Ryoichi ArfArf</h1>
+            </div>
+
+            <div class="d-flex">
+                <div class="pr-5"><strong>127K</strong> posts</div>
+                <div class="pr-5"><strong>24K</strong> followers</div>
+                <div class="pr-5"><strong>1</strong> followings</div>
+            </div>
+
+       </div>         
+    </div>
+
+</div>
+@endsection
